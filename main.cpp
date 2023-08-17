8ca1868f14e8444ed530148f493aa90381454537
bhunia <b21140@students.iitmandi.ac.in>
2023-08-17 18:43:51 +0530

    Third commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 #include "functions.h"
 using namespace std;
 int main(){
+    bool flg=true;
+    while (flg){
     print_hello();
+    int n;
+    cin>>n;
+    if (n<0) {cout<<"-ve enter again"<<endl; continue;}
+    flg = false;
     cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    cout<<"The factorial of"<<n<<" is "<< factorial(n)<< endl;
     return 0;
+
+    }
 }
